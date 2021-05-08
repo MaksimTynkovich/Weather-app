b25d8984192a8d342fc21506acf090783fa5ef02
MaxTynkovich <m_wildfires@mail.ru>
2021-05-08 15:50:21 +0300

    Adding dateBuilder

--- App.js
+++ App.js
@@ -7,10 +7,22 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
+const dateBuilder = (d) => {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app</Text>
       <StatusBar style="auto" />
     </View>
   );
